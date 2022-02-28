f64a9e570a924b6b36cde400536b41bec21c06b5
Irene2607 <lalejandro@est.ups.edu.ec>
2022-02-27 20:12:59 -0500

    conexion base de datos

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import morgan from 'morgan'
 import pkg from '../package.json'
+import productsRoutes from './routes/products.routes'
 
 
 const app = express()
+
+
 
 app.set('pkg', pkg);
 
@@ -17,4 +20,7 @@ app.get('/', (req, res) =>{
         version: app.get('pkg').version
     })
 })
+
+
+app.use('/products', productsRoutes)
 export default app;
